bbcb9a067a19f88e621dac94d6ac5912c4a2eb64
Kaito Udagawa <umireon@gmail.com>
2017-08-13 14:22:48 +0900

    feat: test.sh

---
+++ test.sh
@@ -0,0 +1,11 @@
+# `GH_TOKEN` should be exported as environment variable
+export NPM_TOKEN=null
+export CI=true
+VERSION=1.0.2
+
+# mock semantic-release pre
+sed -i '' -e "s/0.0.0-development/$VERSION/" package.json
+
+node_modules/.bin/semantic-release post
+
+sed -i '' -e "s/$VERSION/0.0.0-development/" package.json
